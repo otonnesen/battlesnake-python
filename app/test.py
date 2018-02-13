cdc48fc7deff84b97601335b72dca1579ed998c5
Oliver Tonnesen <oliver@tonnesen.com>
2018-02-12 16:47:09 -0800

    Pathfinding (sort of) functional in test.py

--- app/test.py
+++ app/test.py
@@ -5,43 +5,41 @@ data = json.load(open('data.json'))
 
 game_board = board(data['width'],data['height'],data['you'],data['snakes'])
 
-# Probably need to store paths as separate entities
-
-def BFS(board, start, depth = 5):
+def pathFind(board, start, depth = 5):
+    current = start
     tmp = []
     stack = []
-    stack.insert(0,start)
-    out = []
-    for count in range(depth):
-        tmp = neighbors(stack[-1])
-        for i in tmp:
-            if i not in out and i not in stack:
-                stack.insert(0, i)
-        out.append(stack.pop())
-
-    
-    return out
+    for n in neighbors(start):
+        for i in range(depth):
+            for j in neighbors(current):
+                if(spaceOK(board, j)):
+                    tmp.append(j)
+                    current = j
+                    break
+        stack.append(tmp)
+        tmp = []
+    return stack
 
 # Returns list of coordinates to the left, right, above, and below a given set of coordinates
 def neighbors(coords):
     return [{'x':coords['x']-1, 'y':coords['y']},{'x':coords['x']+1, 'y':coords['y']},{'x':coords['x'], 'y':coords['y']-1},{'x':coords['x'], 'y':coords['y']+1}]
 
 # Boolean function
-# returns true if coordinates given are OK for the given board,
-# false if a snake or wall exists in the same set of coordinates
+# returns True if coordinates given are OK for the given board,
+# False if a snake or wall exists in the same set of coordinates
 def spaceOK(board, coord):
-    for i in data['snakes']:
+    for i in data['snakes']['data']:
 
         if(data['you']['body']['data'][0] == i['body']['data'][0] and data['you']['length'] > i['length']):
-                return true
+                return True
         for j in i['body']['data']:
             if(coord == j):
-                return false
-    if(coord['x'] < 0 or coord['x'] > board['width'] - 1 or coord['y'] < 0 or coord['y'] > board['height']):
-        return false
+                return False
+    if(coord['x'] < 0 or coord['x'] > board.width - 1 or coord['y'] < 0 or coord['y'] > board.height):
+        return False
             
-    return true
+    return True
 
 print({'x':14,'y':12})
-for i in DFS(game_board, {'x':14,'y':12}):
+for i in pathFind(game_board, {'x':14,'y':12}):
     print(i)
