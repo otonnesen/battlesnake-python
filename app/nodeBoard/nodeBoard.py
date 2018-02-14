13f7ac9a647e8a5199e1f9c6c9cb4170e30cec54
Oliver <oliver@tonnesen.com>
2018-02-13 20:23:34 -0800

    Comment with plans for new board class

--- app/nodeBoard/nodeBoard.py
+++ app/nodeBoard/nodeBoard.py
@@ -1,6 +1,14 @@
 from LinkedList import LinkedList
 class board:
 
+    # Board is an array of LinkedLists.
+    # The plan is to store dictionaries 
+    # containing information pertaining to
+    # the board (such as snakes, food, paths, etc)
+    # instead of integers. For now, the datatype is
+    # nearly identical to that used by the original
+    # board class.
+
     def __init__(self, data):
         self.width = data['width']
         self.height = data['height']
@@ -24,3 +32,6 @@ class board:
     def plot(self, coords, value):
         if(coords['x'] < self.width and coords['x'] >= 0 and coords['y'] < self.height and coords['y'] >= 0):
             self.board[coords['x']].set(coords['y'], value)
+
+    def populateBoard(self):
+
