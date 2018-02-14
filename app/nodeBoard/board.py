7578bcbe54f2ec66452fb997477b5876ad8ad6ab
Oliver <oliver@tonnesen.com>
2018-02-14 03:53:02 -0800

    Set up visual board string methods

--- app/nodeBoard/board.py
+++ app/nodeBoard/board.py
@@ -1,13 +1,15 @@
-from LinkedList import LinkedList
 class board:
 
     def __init__(self, data):
-        self.food = data['food']
+        self.food = data['food']['data']
         self.width = data['width']
         self.height = data['height']
-        self.board = [[{'food':False,'snake':False} for y in range(self.height)] for x in range(self.width)]
+        self.board = [[{'food':False,'snake':None} for y in range(self.height)] for x in range(self.width)]
         self.you = data['you']
-        self.snakes = data['snakes']
+        self.snakes = data['snakes']['data']
+        for i in self.snakes:
+            if(i['id'] == self.you['id']):
+                self.snakes.remove(i)
 
     # Full board
     def __str__(self):
@@ -26,13 +28,19 @@ class board:
         tmp = []
         for y in range(self.height):
             for x in range(self.width):
-                if(self.board[x][y]['snake']):
-                    tmp.append(1)
-                else:
+                if(self.board[x][y]['snake'] == None):
                     tmp.append(0)
-            s.append(str(tmp)+'\n')
+                elif(self.board[x][y]['snake'] == 'body'):
+                    tmp.append(1)
+                elif(self.board[x][y]['snake'] == 'head'):
+                    tmp.append(2)
+                elif(self.board[x][y]['snake'] == 'bodyYou'):
+                    tmp.append(3)
+                elif(self.board[x][y]['snake'] == 'headYou'):
+                    tmp.append(4)
+            s.append(tmp)
             tmp = []
-        return ''.join(s)
+        return s
 
     # Only food board
     def getFoodBoard(self):
@@ -44,10 +52,43 @@ class board:
                     tmp.append(1)
                 else:
                     tmp.append(0)
-            s.append(str(tmp)+'\n')
+            s.append(tmp)
+            tmp = []
+        return s
+
+    # Prints out above function
+    def snakeBoardString(self):
+        s = []
+        tmp = []
+        for y in range(self.height):
+            for x in range(self.width):
+                if(self.board[x][y]['snake'] == None):
+                    tmp.append('_')
+                elif(self.board[x][y]['snake'] == 'body'):
+                    tmp.append('B')
+                elif(self.board[x][y]['snake'] == 'head'):
+                    tmp.append('H')
+                elif(self.board[x][y]['snake'] == 'bodyYou'):
+                    tmp.append('y')
+                elif(self.board[x][y]['snake'] == 'headYou'):
+                    tmp.append('Y')
+            s.append(''.join(tmp)+'\n')
             tmp = []
         return ''.join(s)
 
+    # Prints out above function
+    def foodBoardString(self):
+        s = []
+        tmp = []
+        for y in range(self.height):
+            for x in range(self.width):
+                if(self.board[x][y]['food']):
+                    tmp.append('F')
+                else:
+                    tmp.append('_')
+            s.append(''.join(tmp)+'\n')
+            tmp = []
+        return ''.join(s)
     # Fills the board with empty dictionaries
     def plot(self, coords, field, value):
         if(coords['x'] < self.width and coords['x'] >= 0 and coords['y'] < self.height and coords['y'] >= 0):
@@ -67,8 +108,12 @@ class board:
 
     # Sets food/snake values for the full board
     def populateBoard(self):
-        for snake in self.snakes['data']:
-            for i in snake['body']['data']:
-                self.setSnake(i, True)
-        for i in self.food['data']:
+        for snake in self.snakes:
+            self.setSnake(snake['body']['data'][0], 'head')
+            for i in snake['body']['data'][1:]:
+                self.setSnake(i, 'body')
+        self.setSnake(self.you['body']['data'][0], 'headYou')
+        for i in self.you['body']['data'][1:]:
+            self.setSnake(i, 'bodyYou')
+        for i in self.food:
             self.setFood(i, True)
--- app/nodeBoard/test.py
+++ app/nodeBoard/test.py
@@ -1,13 +1,16 @@
 from board import board
 import json
+import time
+
+start = time.time()
 
 data = json.load(open('data2.json'))
-
 game_board = board(data)
-
 game_board.populateBoard()
 
-print('Food Board')
-print(game_board.getFoodBoard())
-print('\nSnake Board')
-print(game_board.getSnakeBoard())
+end = time.time()
+
+print(game_board.snakeBoardString())
+print(game_board.foodBoardString())
+
+print('Set up board in '+str(end-start)+' seconds')
