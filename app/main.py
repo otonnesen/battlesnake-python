b7f788b36ff3ad3aa436cd14ae210e1a6736efba
Alex McRae <alex@alexandermcrae.com>
2018-03-03 13:01:01 -0800

    fixed

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def start():
     board_height = data['height']
 
 
-    head_url = '%s://%s/static/head.png' % (
+    head_url = '%s://%s/static/head.gif' % (
         bottle.request.urlparts.scheme,
         bottle.request.urlparts.netloc
     )
